99b45a114696d18469710fd77c73e8e797cb4327
JSpiner <jspiner@naver.com>
2016-09-27 06:53:32 +0000

    fix gitignore, change to flask

--- Bot/app.py
+++ Bot/app.py
@@ -1,3 +1,34 @@
 from celery_worker import worker
+ 
+from flask import Flask
+from flask import Response
+from flask import request
+import json 
 
-worker.delay(random.randint(0,100))
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    html = "<html> <body> <a href='http://naver.com'>슬랙 연결</a> </body> </html>"
+    return html
+
+@app.route('/slack/oauth', methods = ['POST'])
+def slack_oauth():
+
+    return ''
+
+@app.route('/slack/event', methods = ['POST'])
+def slack_event():
+    payload = request.get_data().decode()
+    data = json.loads(payload) 
+
+    response = {}
+
+    response['challenge'] = data['challenge']
+
+    return Response(json.dumps(response), mimetype='application/x-www-form-urlencoded')
+
+
+ssl_context = ('ssoma.xyz.crt', 'ca.key')
+
+app.run(host='0.0.0.0', debug='True', port = 99, ssl_context = ssl_context)
