ee487d6409429d4b5a5c3471fab2bd77e8aa10a0
JSpiner <jspiner@naver.com>
2016-09-27 11:31:50 +0000

    add oauth request

--- Bot/app.py
+++ Bot/app.py
@@ -3,19 +3,42 @@ from celery_worker import worker
 from flask import Flask
 from flask import Response
 from flask import request
+import requests
 import json 
 
 app = Flask(__name__)
 
+#load josn key file
+with open('key.json', 'r') as f:
+    key = json.load(f)
+
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    html = "<html> <body> <a href='http://naver.com'>슬랙 연결</a> </body> </html>"
+
+    url = ("https://slack.com/oauth/authorize?client_id="
+        +key['slackapp']['client_id']
+        +"&scope=team:read+channels:read+channels:history")
+
+    html = "<html> <body> <a href='"+url+"'>슬랙 연결</a> </body> </html>"
+
     return html
 
 @app.route('/slack/oauth', methods = ['POST'])
 def slack_oauth():
+    code = request.args.get('code')
+    r = requsts.post("https://slack.com/api/oauth.access", 
+        data = {
+            'client_id'     : key['slackapp']['client_id'], 
+            'client_secret' : key['slackapp']['client_secret'], 
+            'code'          : code
+        }
+    )
 
-    return ''
+    response = json.loads(r.text)
+
+    access_token = response['access_token']
+
+    return 'auth success'
 
 @app.route('/slack/event', methods = ['POST'])
 def slack_event():
@@ -24,7 +47,8 @@ def slack_event():
 
     response = {}
 
-    response['challenge'] = data['challenge']
+    if 'challenge' in response:
+        response['challenge'] = data['challenge']
 
     return Response(json.dumps(response), mimetype='application/x-www-form-urlencoded')
 
