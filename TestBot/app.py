cb3bdc3cea4bd4ee4fa78660b80273395a7affe1
JSpiner <jspiner@naver.com>
2016-10-04 15:06:21 +0000

    add slackclient

--- TestBot/app.py
+++ TestBot/app.py
@@ -7,23 +7,37 @@ import json
 with open('key.json', 'r') as f:
     key = json.load(f)
 
-def tester_work(api_token):
+def tester_work(processId, api_token):
+    print("process : " + str(processId) + " run with : " + api_token)
 
-    print(api_token)
+    sc = SlackClient(api_token)
+    if sc.rtm_connect():
+        while True:
+            response = sc.rtm_read()
+
+            if len(response) == 0: 
+                continue
+
+            # response는 배열로, 여러개가 담겨올수 있음
+            for data in response:
+                print(data)
+
+                if ('type' in data) is False:
+                    continue	
 
     return
 
 testerProcess1 = Process(
     target=tester_work, 
-    args=(key['slackbots'][0]['api_token'])
+    args=(1, key['slackbots'][0]['api_token'])
 )
 testerProcess2 = Process(
     target=tester_work, 
-    args=(key['slackbots'][1]['api_token'])
+    args=(2, key['slackbots'][1]['api_token'])
 )
 testerProcess3 = Process(
     target=tester_work, 
-    args=(key['slackbots'][2]['api_token'])
+    args=(3, key['slackbots'][2]['api_token'])
 )
 
 
