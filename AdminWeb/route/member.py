00aad86000619e39ea4a569df6f9c08104f0b1ba
JSpiner <jspiner@naver.com>
2016-10-09 04:02:08 +0000

    add gameResult page #35

--- AdminWeb/route/member.py
+++ AdminWeb/route/member.py
@@ -128,6 +128,27 @@ class Members(MethodView):
                 # logging.warning(str(e))
                 return json.dumps(static.RES_DEFAULT(400,"err"),sort_keys=True, indent = 4)
 
+        elif types == "getAllGameResult":
+
+            try:
+                game_id = request.args.get('gameId')
+
+                print("[ADMIN]_GET_ALL Games")            
+                conn = db_manager.engine.connect()
+                result = conn.execute(
+                    "SELECT * FROM GAME_RESULT where  game_id=%s",
+                    (game_id)
+                )
+                conn.close()
+                rows =util.fetch_all_json(result)                
+                
+                return json.dumps(static.RES_DEFAULT(200,rows),sort_keys=True, indent = 4)
+
+            except Exception as e:
+                print(str(e))
+                # logging.warning(str(e))
+                return json.dumps(static.RES_DEFAULT(400,"err"),sort_keys=True, indent = 4)
+
     
     def post(self,types):
         print("post")
