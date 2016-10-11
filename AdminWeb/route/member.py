47827d0cd20d9cc881668ade429d7751cb79a9f4
JSpiner <jspiner@naver.com>
2016-10-11 11:16:11 +0000

    add team list #62

--- AdminWeb/route/member.py
+++ AdminWeb/route/member.py
@@ -68,6 +68,70 @@ class Members(MethodView):
                 print(str(e))
                 # logging.warning(str(e))
                 return json.dumps(static.RES_DEFAULT(400,"err"),sort_keys=True, indent = 4)
+        elif types == "getTeamInfo":
+            try:
+                teamId = request.args.get('team_id')
+
+                print("[ADMIN]getTeamInfo With channelID")    
+
+                conn = db_manager.engine.connect()
+                result = ""
+                # print("channelID= "+channel_id)
+               
+                result = conn.execute(
+                    "SELECT *,  ( "
+                    "    SELECT "
+                    "        count(user_id) "
+                    "    FROM USER  "
+                    "    where "
+                    "        USER.team_id = TEAM.team_id " 
+                    ") as user_num, "
+                    "( "
+                    "    SELECT " 
+                    "        count(team_id) "
+                    "        FROM GAME_INFO "
+                    "    where "
+                    "        GAME_INFO.team_id = TEAM.team_id " 
+                    ") as game_num, "
+                    "( "
+                    "    SELECT " 
+                    "        AVG(score) "
+                    "        FROM GAME_RESULT " 
+                    "    INNER JOIN USER  "
+                    "    ON USER.user_id = GAME_RESULT.user_id " 
+                    "    where "
+                    "        USER.team_id = TEAM.team_id " 
+                    ") as avg_score, "
+                    "( "
+                    "    SELECT " 
+                    "        MAX(score) "
+                    "        FROM GAME_RESULT " 
+                    "    INNER JOIN USER  "
+                    "    ON USER.user_id = GAME_RESULT.user_id " 
+                    "    where "
+                    "        USER.team_id = TEAM.team_id " 
+                    ") as max_score, "
+                    "( "
+                    "    SELECT " 
+                    "        MAX(start_time) "
+                    "        FROM GAME_INFO  "
+                    "    where "
+                    "        GAME_INFO.team_id = TEAM.team_id " 
+                    ") as recent_play_time "
+                    "from TEAM "
+                    "where TEAM.team_id = %s; ",
+                    teamId
+                )
+                
+                conn.close()
+                rows = util.fetch_all_json(result)                
+                
+                return json.dumps(static.RES_DEFAULT(200,rows),sort_keys=True, indent = 4)
+            
+            except Exception as e:
+                print(str(e))
+                # logging.warning(str(e))
+                return json.dumps(static.RES_DEFAULT(400,"err"),sort_keys=True, indent = 4)
         elif types == "getAllTeam":
 
             try:
