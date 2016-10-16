befd8441d568d031c2d2d89a47072e11d860db6a
yenoss <cpg0504@gmail.com>
2016-10-16 00:59:30 +0000

    #85 [batch] dailty inactive checker

--- DailyWoker/app.py
+++ DailyWoker/app.py
@@ -13,74 +13,120 @@ engine = create_engine('mysql+pymysql://'+conf["mysql"]["user"]+':'+conf["mysql"
 
 
 def fetch_all_json(result):
-  lis = []
+    lis = []
 
-  for row in result.fetchall():
-    i =0
-    dic = {}  
-    
-    for data in row:
-      # if(len(result.keys())
-      # print(len(result.keys()))
-      # print(i)
-      # print(data)
-      dic[result.keys()[i]]= str(data)
-      if i == len(row)-1:
-        lis.append(dic)
+    for row in result.fetchall():
+	    i =0
+	    dic = {}  
+	    
+	    for data in row:
+	      # if(len(result.keys())
+	      # print(len(result.keys()))
+	      # print(i)
+	      # print(data)
+	        dic[result.keys()[i]]= str(data)
+	        if i == len(row)-1:
+	            lis.append(dic)
 
-      i=i+1
-  return lis   
+	        i=i+1
+    return lis   
 
 def updateUserActive():
+	print('udate')
 
-    try:
+	# active 유저로만들기!
+	try:
 		conn = engine.connect()
-		trans = conn.begin()
-		# 유
-		result = conn.execute(
-		    "select u.user_id from USER as u inner join GAME_RESULT as gr on u.user_id = gr.user_id inner join GAME_INFO as gi on gi.game_id = gr.game_id where gi.end_time <DATE_SUB(CURDATE() , INTERVAL 7 DAY) group by u.user_id"
-		)
 
+		trans = conn.begin()
+		result = conn.execute(
+			"select * from (select MAX(gi.end_time) as recentTime,u.user_id from USER as u inner join GAME_RESULT as gr on u.user_id = gr.user_id inner join GAME_INFO as gi on gi.game_id = gr.game_id group by u.user_id) as rt where rt.recentTime > DATE_SUB(CURDATE() , INTERVAL 7 DAY)"
+		)
 		rows= fetch_all_json(result)
 
 		arrQueryString=[]
-		arrQueryString.append('update USER set USER.isActive = 0 where ')
+		arrQueryString.append('update USER set USER.isActive = 1 where ')
 
 		for row in rows:
-			print(row)
 			arrQueryString.append('USER.user_id = "'+ row['user_id']+ '"')
 			arrQueryString.append(' or ')
 
 		arrQueryString.pop()
 		lastQuery = "".join(arrQueryString)
 
-		trans = conn.begin()
+		
 		result = conn.execute(
-		    lastQuery
+			lastQuery
 		)
 
 		arrQueryString=[]
-		arrQueryString.append('insert into USER_DAILY_ACTIVE (date,user_id) values ')
+		arrQueryString.append('insert into USER_DAILY_ACTIVE (date,user_id,active) values ')
 		for row in rows:
-			arrQueryString.append('(curdate(),"'+ row['user_id']+ '")')
+			arrQueryString.append('(curdate(),"'+ row['user_id']+ '",1)')
 			arrQueryString.append(',')
-			
+
 		arrQueryString.pop()
 		lastQuery = "".join(arrQueryString)
 		print(lastQuery)
+
 		conn.execute(
-		    lastQuery
+			lastQuery
 		)
 		trans.commit()
 		conn.close() 
+	except Exception as e:
+		print(str(e))
 
-    except Exception as e:
-    	print(str(e))
+	try:
+		conn = engine.connect()
+
+		trans = conn.begin()
+		result = conn.execute(
+			"select * from (select MAX(gi.end_time) as recentTime,u.user_id from USER as u inner join GAME_RESULT as gr on u.user_id = gr.user_id inner join GAME_INFO as gi on gi.game_id = gr.game_id group by u.user_id) as rt where rt.recentTime < DATE_SUB(CURDATE() , INTERVAL 7 DAY)"
+		)
+		rows= fetch_all_json(result)
+		print(rows)
+		if len(rows)==0:
+			trans.commit()
+			conn.close() 
+		else :				
+			arrQueryString=[]
+			arrQueryString.append('update USER set USER.isActive = 0 where ')
+
+			for row in rows:
+				arrQueryString.append('USER.user_id = "'+ row['user_id']+ '"')
+				arrQueryString.append(' or ')
+
+			arrQueryString.pop()
+			lastQuery = "".join(arrQueryString)
+
+			
+			result = conn.execute(
+				lastQuery
+			)
+
+			arrQueryString=[]
+			arrQueryString.append('insert into USER_DAILY_ACTIVE (date,user_id,active) values ')
+			for row in rows:
+				arrQueryString.append('(curdate(),"'+ row['user_id']+ '",0)')
+				arrQueryString.append(',')
+
+			arrQueryString.pop()
+			lastQuery = "".join(arrQueryString)
+			print(lastQuery)
+
+			conn.execute(
+				lastQuery
+			)
+			trans.commit()
+			conn.close() 
+	except Exception as e:
+		print(str(e))		
 
 
 def job():
-    print("working well!")
-    updateUserActive()
+	print("working well!")
+	# updateUserActive()
 
 
 # 10분마다 
@@ -89,8 +135,7 @@ def job():
 # schedule.every().hour.do(job)
 # 매일 특정 시간에
 schedule.every().day.at("00:00").do(job)
-
-
+updateUserActive()
 
 
 
