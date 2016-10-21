168a12c2a87912dfd4dc85dc8a53ca6742e95b8d
JSpiner <jspiner@naver.com>
2016-10-21 16:09:14 +0900

    define rtm manager func

--- Common/manager/rtm_manager.py
+++ Common/manager/rtm_manager.py
@@ -1,1 +1,12 @@
- 
+from redis_manager import redis_client
+ #rtm socket manager
+
+ def start_rtm(teamId):
+     return 0
+    
+def _is_socket_opened(teamId):
+    redis_client.get()
+    return False
+
+def _open_new_socket(teamId):
+    return 0
