3240f7ee7d9ccef7d28c5792eff440d9884a8865
JSpiner <jspiner@naver.com>
2016-11-03 17:13:42 +0900

    connect error bot with newrelic #152

--- ErrorBot/app.py
+++ ErrorBot/app.py
@@ -5,40 +5,50 @@ from flask import request
 
 import json
 import requests
+import random
 
 app = Flask(__name__)
 
 #load josn key file
 with open('key.json', 'r') as f:
-    key = json.load(f)
-
+    key = json.load(f) 
+ 
 """
-ex)
-{
+ex) 
+{ 
     "level"     : "error",
     "filename"  : "app.py",
     "lineno"    : 85,
     "asctime"   : 14051874,
     "message"   : "db exception : SQL Syntex error"
 }
-"""
+""" 
 
 @app.route('/logging', methods = ['POST'])
 def logging():
-    data = json.loads(request.form.get('data'))
+    data = json.loads(request.form.get('alert'))
     print(data)
+    print(data['message'])
+    print(data['long_description'])
     webhookurl = key['errorbot']['webhook_url']
  
-    text = "` [%s|%s:%s] ` %s : %s " % (data['level'], data['filename'], data['lineno'], data['asctime'], data['message'])
+#    text = "` [%s|%s:%s] ` %s : %s " % (data['level'], data['filename'], data['lineno'], data['asctime'], data['message'])
+    texts = [
+        "에러 발견!! 코딩 똑바로 안함???",
+        "또 에러났어;;;",
+        "에러좀 작작내라",
+        "코드보고 반성좀 해라"
+    ]
     params = {
         'payload'   : json.dumps(
             {
-                'text'  : '에러 발견!',
+                'text'  : random.choice(texts),
                 'attachments'   : 
                 [
                     {
-                        "color": "#439FE0",
-                        "text"  : text
+                        "color" : "#439FE0",
+                        "title" : data['message'],
+                        "text"  : data['long_description']
                     }
                 ]
                 
