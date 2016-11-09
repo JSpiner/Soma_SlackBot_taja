62c8a80b16375fadb0db301b72d940a57547242e
JSpiner <jspiner@naver.com>
2016-11-09 21:46:24 +0900

    fix celery run

--- Bot/celery_run.sh
+++ Bot/celery_run.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-NEW_RELIC_CONFIG_FILE=../newrelic_celery.ini newrelic-admin run-program celery -A celery_worker worker --loglevel=info
+NEW_RELIC_CONFIG_FILE=../newrelic.ini newrelic-admin run-program celery -A celery_worker worker --loglevel=info
